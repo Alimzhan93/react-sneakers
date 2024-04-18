91f4a097ab4e7f41618098faaf9a29c713cd2d62
77001 <alimzhanzhakashev@gmail.com>
2024-04-18 17:52:03 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const arr = [
+  { name: "кроссовки Nike", price: 12500, UrlImage: "/img/sneakers/1.jpg" },
+  { name: "кроссовки Nike", price: 55555, UrlImage: "/img/sneakers/2.jpg" },
+  { name: "кроссовки Nike", price: 55888, UrlImage: "/img/sneakers/3.jpg" },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
-      
-       {/* Drawer */}
-        <Drawer/>
-      
+      {/* Drawer */}
+      <Drawer />
+
       {/* Header */}
-        <Header/>
+      <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-4o">
           <h1>Все кроссовки</h1>
@@ -19,11 +24,14 @@ function App() {
             <input placeholder="Поиск..." />
           </div>
         </div>
-
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card 
+            title={obj.name}
+            price={obj.price}
+            UrlImage={obj.UrlImage}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="favorite">
         <img src="/img/unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.UrlImage} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12050 тг.</b>
+          <b>{props.price} тг.</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
